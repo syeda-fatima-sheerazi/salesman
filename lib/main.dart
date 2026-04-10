f131ed5e09e0cc6e1ba2009bf8eb1cd45454b34e
syeda-fatima.dev <syedafatima.dev@gmail.com>
2026-04-10 12:21:53 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: AppTheme.light,
           darkTheme: AppTheme.dark,
-          themeMode: ThemeMode.light,
+          themeMode: ThemeMode.dark,
           home: DashboardView(),
         );
       },
