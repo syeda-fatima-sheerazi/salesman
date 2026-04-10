1e323c2f0a5954d6463d1fb39376a3dbd7149a9d
syeda-fatima.dev <syedafatima.dev@gmail.com>
2026-04-10 12:17:10 -0700

    moved all dummy data of shop's to servicess

--- lib/core/screens/home/home_controller.dart
+++ lib/core/screens/home/home_controller.dart
@@ -1,120 +1,45 @@
 import 'package:get/route_manager.dart';
 import 'package:get/state_manager.dart';
+import 'package:practices/core/enums/data_state.dart';
 import 'package:practices/core/models/shop.dart';
 import 'package:practices/core/screens/home/detailed/detailed_view.dart';
+import 'package:practices/core/services/shop_service.dart';
+import 'package:practices/core/services/location_service.dart';
 
 class HomeController extends GetxController {
-  RxString selectedDistrict = "".obs;
-  RxString selectedTown = "".obs;
-  RxString selectedArea = "".obs;
-  final RxList<Shop?> shops = <Shop?>[].obs;
+  final RxString selectedDistrict = "".obs;
+  final RxString selectedTown = "".obs;
+  final RxString selectedArea = "".obs;
+  final RxList<Shop?> shopList = <Shop?>[].obs;
+  final Rx<DataState> dataState = DataState.empty.obs;
 
-  List<String> districts = ['Karachi', 'Lahore', 'Islamabad'];
-  Map<String, dynamic> towns = {
-    'Karachi': ['Gulshan-e-Iqbal', 'Saddar', 'North Nazimabad'],
-    'Lahore': ['Model Town', 'Gulberg'],
-    'Islamabad': ['F-6', 'G-10'],
-  };
-  Map<String, dynamic> areas = {
-    'Gulshan-e-Iqbal': ['Block 1', 'Block 2'],
-    'Saddar': ['Area A', 'Area B'],
-    'North Nazimabad': ['Sector 1', 'Sector 2'],
-    'Model Town': ['Zone 1'],
-    'Gulberg': ['Zone A'],
-    'F-6': ['Sector F6/1'],
-    'G-10': ['Sector G10/2'],
-  };
+  List<String> districts = [];
+  Map<String, dynamic> towns = {};
+  Map<String, dynamic> areas = {};
+
+  final ShopService _shopService = ShopService();
+  final LocationService _locationService = LocationService();
 
   @override
   void onInit() {
     super.onInit();
+    _loadLocationData();
     loadDummyShops();
   }
 
-  void loadDummyShops() {
-    shops.assignAll([
-      Shop(
-        isVisited: true,
-        shopName: 'Shop 1',
-        shopOwner: 'Owner 1',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 1',
-      ),
-      Shop(
-        shopName: 'Shop 2',
-        shopOwner: 'Owner 2',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 2',
-      ),
-      Shop(
-        shopName: 'Shop 3',
-        shopOwner: 'Owner 3',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 3',
-      ),
-      Shop(
-        shopName: 'Shop 4',
-        shopOwner: 'Owner 4',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 4',
-      ),
-      Shop(
-        shopName: 'Shop 5',
-        shopOwner: 'Owner 5',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 5',
-      ),
-      Shop(
-        shopName: 'Shop 6',
-        shopOwner: 'Owner 6',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 6',
-      ),
-      Shop(
-        shopName: 'Shop 7',
-        shopOwner: 'Owner 7',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 7',
-      ),
-      Shop(
-        shopName: 'Shop 8',
-        shopOwner: 'Owner 8',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 8',
-      ),
-      Shop(
-        shopName: 'Shop 9',
-        shopOwner: 'Owner 9',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 9',
-      ),
-      Shop(
-        shopName: 'Shop 10',
-        shopOwner: 'Owner 10',
-        cellPhone: '0303684795739',
-        shopImagUrl:
-            'https://img.freepik.com/premium-vector/shops-stores-icons-set-flat-design-style-vector-illustration_498048-1862.jpg?semt=ais_hybrid&w=740&q=80',
-        address: 'Address 10',
-      ),
-    ]);
+  Future<void> _loadLocationData() async {
+    dataState.value = DataState.loading;
+    districts = await _locationService.fetchDistricts();
+    towns = await _locationService.fetchTowns();
+    areas = await _locationService.fetchAreas();
+    dataState.value = DataState.loaded;
+    update();
+  }
+
+  Future<void> loadDummyShops() async {
+    final shops = await _shopService.fetchDummyShops();
+    shopList.assignAll(shops);
+    update();
   }
 
   void gotoDetailedView() {
