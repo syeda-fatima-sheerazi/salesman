757386f73ed99e96119f113ac6e90ca303fd32fb
syeda-fatima.dev <syedafatima.dev@gmail.com>
2026-07-01 12:07:18 -0700

    user model updated

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -2,15 +2,16 @@ class UserModel {
   final String id;
   final String name;
   final String email;
+  final String password;
   final String avatarUrl;
 
   UserModel({
     required this.id,
     required this.name,
     required this.email,
+    this.password = '',
     this.avatarUrl = 'assets/icons/user.png',
-  })
-  ; 
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
@@ -26,6 +27,26 @@ class UserModel {
       'id': id,
       'name': name,
       'email': email,
+      'avatarUrl': avatarUrl,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+      avatarUrl: map['avatarUrl'] ?? 'assets/icons/user.png',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
       'avatarUrl': avatarUrl,
     };
   }
